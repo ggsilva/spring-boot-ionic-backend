8ee522a7371a820db85354b18cf0dea6e02a89ae
Guilherme Silva <gg-silva@outlook.com>
2019-07-14 21:31:44 -0300

    Testando primeiro dominio - categoria

--- src/main/java/com/ggs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ggs/cursomc/resources/CategoriaResource.java
@@ -2,16 +2,31 @@ package com.ggs.cursomc.resources;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ggs.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = GET)
-	public String listar() {
-		return "Esta ok";
+	public List<Categoria> listar() {
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(newCategoria(1, "Informatica"));
+		lista.add(newCategoria(2, "Escritorio"));
+		return lista;
+	}
+
+	private static Categoria newCategoria(Integer id, String nome) {
+		Categoria c1 = new Categoria();
+		c1.setId(id);
+		c1.setNome(nome);
+		return c1;
 	}
 
 }
