5d2f59621ce265c2c6153465bfcf3ef9367de0b0
Guilherme Silva <gg-silva@outlook.com>
2019-07-15 21:32:26 -0300

    Estado e cidade

--- src/main/java/com/ggs/cursomc/CursomcApplication.java
+++ src/main/java/com/ggs/cursomc/CursomcApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ggs.cursomc.domain.Categoria;
+import com.ggs.cursomc.domain.Cidade;
+import com.ggs.cursomc.domain.Estado;
 import com.ggs.cursomc.domain.Produto;
 import com.ggs.cursomc.repositories.CategoriaRepository;
+import com.ggs.cursomc.repositories.CidadeRepository;
+import com.ggs.cursomc.repositories.EstadoRepository;
 import com.ggs.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
+	@Autowired CidadeRepository cidadeRepo;
+	@Autowired EstadoRepository estadoRepo;
 	@Autowired ProdutoRepository produtoRepo;
 	@Autowired CategoriaRepository categoriaRepo;
 
@@ -24,6 +30,11 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		createCategorias();
+		createEstados();
+	}
+
+	private void createCategorias() {
 		Categoria c1 = newCategoria(null, "Informática");
 		Categoria c2 = newCategoria(null, "Escritório");
 		
@@ -55,6 +66,37 @@ public class CursomcApplication implements CommandLineRunner {
 		c.setId(id);
 		c.setNome(nome);
 		return c;
+	}
+
+	private void createEstados() {
+		Estado e1 = newEstado(null, "Minas Gerais");
+		Estado e2 = newEstado(null, "Paraná");
+		
+		Cidade c1 = newCidade(null, "Uberlândia", e1);
+		Cidade c2 = newCidade(null, "Maringá", e2);
+		Cidade c3 = newCidade(null, "Curitiba", e2);
+		
+		e1.getCidades().add(c1);
+		e2.getCidades().add(c2);
+		e2.getCidades().add(c3);
+		
+		estadoRepo.save(asList(e1, e2));
+		cidadeRepo.save(asList(c1, c2, c3));
+	}
+	
+	private static Cidade newCidade(Integer id, String nome, Estado estado) {
+		Cidade c = new Cidade();
+		c.setId(id);
+		c.setNome(nome);
+		c.setEstado(estado);
+		return c;
+	}
+	
+	private static Estado newEstado(Integer id, String nome) {
+		Estado e = new Estado();
+		e.setId(id);
+		e.setNome(nome);
+		return e;
 	}
 
 }
