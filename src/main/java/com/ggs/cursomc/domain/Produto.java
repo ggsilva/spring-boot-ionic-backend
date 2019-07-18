ab66ec2cc09505715b9feeb35f14b88db497d251
Guilherme Silva <gg-silva@outlook.com>
2019-07-17 22:00:17 -0300

    Atualização utilizando somente JsonIgnore

--- src/main/java/com/ggs/cursomc/domain/Produto.java
+++ src/main/java/com/ggs/cursomc/domain/Produto.java
@@ -17,7 +17,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -31,8 +30,8 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 	
+	@JsonIgnore
 	@ManyToMany
-	@JsonBackReference
 	@JoinTable(
 			name = "PRODUTO_CATEGORIA", 
 			joinColumns = @JoinColumn(name = "produto_id"),
